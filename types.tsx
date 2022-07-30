18a274062bb17319bcba91cb7aa7de0669cc7dbf
Athirson <athirsonarceus@gmail.com>
2022-07-30 12:57:50 -0300

    Tabs adicionadas

--- types.tsx
+++ types.tsx
@@ -27,6 +27,8 @@ export type RootStackScreenProps<Screen extends keyof RootStackParamList> = Nati
 export type RootTabParamList = {
   Home: undefined;
   Account: undefined;
+  Map: undefined;
+  Books: undefined;
 };
 
 export type RootTabScreenProps<Screen extends keyof RootTabParamList> = CompositeScreenProps<
