9a2439340e2f3fa1a3e5c4d3e52d018ddedc613f
Athirson <athirsonarceus@gmail.com>
2022-07-30 23:49:58 -0300

    Tentando por mapa

--- screens/MapsScreen.tsx
+++ screens/MapsScreen.tsx
@@ -1,13 +1,26 @@
+// import React from 'react';
 import React from 'react';
 import { StyleSheet } from 'react-native';
+import MapView from 'react-native-maps'
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
+
 
 export default function MapsScreen() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tela dos mapas</Text>
+      <MapView style={{
+        flex: 1
+      }}
+      region={{
+        latitude: 42.882004, 
+        longitude: 74.582748, 
+        latitudeDelta: 0.0922, 
+        longitudeDelta: 0.0421
+      }}
+      showsUserLocation={true}/>
     </View>
   );
 }
