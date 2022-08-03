585863f6229857d95d2aa201ecbd68a0c4d0e09b
athirson <athirsonarceus@gmail.com>
2022-08-03 17:31:26 -0300

    Co-authored-by: Guilherme Scarabelli <GuiScarabelli@users.noreply.github.com>

--- screens/MapsScreen.tsx
+++ screens/MapsScreen.tsx
@@ -1,17 +1,39 @@
 // import React from 'react';
 import React from 'react';
-import { StyleSheet, Image } from 'react-native';
+
+import { StyleSheet } from 'react-native';
 import MapView from 'react-native-maps'
 
-import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
+import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
 
 
 export default function MapsScreen() {
-  return (
+  const center = {
+    lat: -3.745,
+    lng: -38.523
+  };
+
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: "AIzaSyBg1UtZII7XxZhq82Arr1noy99uShCP3Bs"
+  })
+
+  const [map, setMap] = React.useState(null)
+
+  const onLoad = React.useCallback(function callback(map) {
+    const bounds = new window.google.maps.LatLngBounds(center);
+    map.fitBounds(bounds);
+    setMap(map)
+  }, [])
+
+  const onUnmount = React.useCallback(function callback(map) {
+    setMap(null)
+  }, [])
+  
+  return isLoaded ? (
     <View style={styles.container}>
-      <Text style={styles.title}>Tela dos mapas</Text>
-      {/* <MapView style={{
+      {/* <MapView style={{ 
         flex: 1,
         width: '100%',
         height: '100%',
@@ -22,8 +44,23 @@ export default function MapsScreen() {
         latitudeDelta: 0.0922, 
         longitudeDelta: 0.0421
       }}/> */}
+      <GoogleMap
+        mapContainerStyle={{
+          width: '100%',
+          height: '100%',
+        }}
+        center={center}
+        zoom={10}
+        onLoad={onLoad}
+        onUnmount={onUnmount}
+      >
+        { /* Child components, such as markers, info windows, etc. */ }
+        <></>
+      </GoogleMap>
     </View>
-  );
+  ) : <View>
+    <Text>Oops</Text>
+  </View>
 }
 
 const styles = StyleSheet.create({
