7d7ab11f9a7af13923a758f96347b2c237d4d88d
Athirson Silva <Athirsonarceus@gmail.com>
2022-09-08 07:16:41 -0300

    HomeScreen component

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,143 +1,16 @@
 import React, { useState } from 'react';
-import { Dimensions, Pressable, SafeAreaView, ScrollView } from 'react-native';
-import styles from '../styles/HomeScreen.style';
-import { Text, View } from '../components/Themed';
+import Home from '../components/HomeScreen';
 import { RootTabScreenProps } from '../types';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'react-native-gesture-handler';
-import { createStackNavigator } from '@react-navigation/stack';
-import { ListGroup, Carousel } from 'react-bootstrap';
+
 
 const HomeScreen = ({ navigation }: RootTabScreenProps<'Home'>) => {
-  const [index, setIndex] = useState(0);
-
-  const handleSelect = (selectedIndex: React.SetStateAction<number>, e: any) => {
-    setIndex(selectedIndex);
-  };
-
-  const exampleImage = require('../assets/images/example.jpeg');
-
   return (
-    <SafeAreaView style={styles.container}> 
-      <ScrollView
-      showsVerticalScrollIndicator={false}
-      >
-      <View style={styles.carousel}>
-        <Carousel fade>
-      <Carousel.Item interval={3000}>
-        <img
-          className="d-block w-100"
-          src={exampleImage}
-          alt="First slide"
-        />
-        <Carousel.Caption>
-          <h3>Destaques</h3>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item interval={3000}>
-        <img
-          className="d-block w-100"
-          src={exampleImage}
-          alt="Second slide"
-        />
-
-        <Carousel.Caption>
-          <h3>Destaques</h3>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item interval={3000}>
-        <img
-          className="d-block w-100"
-          src={exampleImage}
-          alt="Third slide"
-        />
-
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>
-        </View>
-    <View>
-      <Text style={styles.subtitle}>Categorias</Text>
-      <Text style={styles.description}>Escolha entre as categorias de restaurantes</Text>
-    </View>
-      <View style={{ width: 'auto' ,maxWidth: Dimensions.get('screen').width }}>
-    <ScrollView 
-      horizontal={true}
-      showsHorizontalScrollIndicator={false}
-    >
-        <Pressable>
-          <img src={exampleImage} className="rounded-circle" style={{ width: 70, height: 70, marginLeft: 10, marginRight: 10 }}/>
-        </Pressable>
-        <Pressable>
-          <img src={exampleImage} className="rounded-circle" style={{ width: 70, height: 70, marginLeft: 10, marginRight: 10 }}/>
-        </Pressable>
-        <Pressable>
-          <img src={exampleImage} className="rounded-circle" style={{ width: 70, height: 70, marginLeft: 10, marginRight: 10 }}/>
-        </Pressable>
-        <Pressable>
-          <img src={exampleImage} className="rounded-circle" style={{ width: 70, height: 70, marginLeft: 10, marginRight: 10 }}/>
-        </Pressable>
-        <Pressable>
-          <img src={exampleImage} className="rounded-circle" style={{ width: 70, height: 70, marginLeft: 10, marginRight: 10 }}/>
-        </Pressable>
-        <Pressable>
-          <img src={exampleImage} className="rounded-circle" style={{ width: 70, height: 70, marginLeft: 10, marginRight: 10 }}/>
-        </Pressable>
-    </ScrollView>
-      </View>
-    <View>
-      <Text style={styles.subtitle}>Reservados recentemente</Text>
-      <Text style={styles.description}>Estabelecimentos que você visitou recentemente</Text>
-      <ListGroup as='ul'>
-        <ListGroup.Item 
-        as="li"
-        className="d-flex justify-content-between align-items-start"
-        style={{ border: 'none', marginTop: 10, marginBottom: 10 }}
-        onClick={() => navigation.navigate('Operation')}
-      >
-        <img src={exampleImage} className="rounded-circle" style={{ width: 40, height: 40, marginLeft: 10, marginRight: 10 }}/>
-        <div className="ms-2 me-auto">
-          <div className="fw-bold">Bar do Armando</div>
-        </div>
-        </ListGroup.Item>
-        <ListGroup.Item 
-        as="li"
-        className="d-flex justify-content-between align-items-start"
-        style={{ border: 'none', marginTop: 10, marginBottom: 10 }}
-        onClick={() => navigation.navigate('Restaurant')}
-      >
-        <img src={exampleImage} className="rounded-circle" style={{ width: 40, height: 40, marginLeft: 10, marginRight: 10 }}/>
-        <div className="ms-2 me-auto">
-          <div className="fw-bold">Bar do João</div>
-        </div>
-        </ListGroup.Item>
-        <ListGroup.Item 
-        as="li"
-        className="d-flex justify-content-between align-items-start"
-        style={{ border: 'none', marginTop: 10, marginBottom: 10 }}
-      >
-        <img src={exampleImage} className="rounded-circle" style={{ width: 40, height: 40, marginLeft: 10, marginRight: 10 }}/>
-        <div className="ms-2 me-auto">
-          <div className="fw-bold">Bar do Zézin</div>
-        </div>
-        </ListGroup.Item>
-        <ListGroup.Item 
-        as="li"
-        className="d-flex justify-content-between align-items-start"
-        style={{ border: 'none', marginTop: 10, marginBottom: 10 }}
-      >
-        <img src={exampleImage} className="rounded-circle" style={{ width: 40, height: 40, marginLeft: 10, marginRight: 10 }}/>
-        <div className="ms-2 me-auto">
-          <div className="fw-bold">Bar do Edinaldo</div>
-        </div>
-        </ListGroup.Item>
-      </ListGroup>
-    </View>
-    </ScrollView>
-    </SafeAreaView>
-  );
+    <>
+      <Home />
+    </>
+  )
 }
 
 export default HomeScreen
