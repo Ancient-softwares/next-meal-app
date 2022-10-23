9cc3951b16696146c10801c3d5ab2e18d0b9877c
Athirson <athirsonarceus@gmail.com>
2022-10-23 15:12:44 -0300

    fix 1

--- backend/index.ts
+++ backend/index.ts
@@ -1,35 +1,28 @@
-import { Callback } from 'react-native-image-picker';
-
-// Use local .env file for env vars when not deployed
-if (process.env.NODE_ENV !== 'production') {
-	require('dotenv').config();
-}
-
-const multer = require('multer');
-const multerS3 = require('multer-s3');
+const multer = require('multer')
+const multerS3 = require('multer-s3')
 
 // Initialize multers3 with our s3 config and other options
 const upload = multer({
 	storage: multerS3({
 		acl: 'public-read',
 		metadata(req: any, file: any, cb: any) {
-			cb(null, { fieldName: file.fieldname });
+			cb(null, { fieldName: file.fieldname })
 		},
 		key(req: Request, file: File, cb: any) {
-			cb(null, Date.now().toString() + '.png');
+			cb(null, Date.now().toString() + '.png')
 		},
 	}),
-});
+})
 
 // Expose the /upload endpoint
-const app = require('express')();
-const http = require('http').Server(app);
+const app = require('express')()
+const http = require('http').Server(app)
 
 app.post('/upload', upload.single('photo'), (req: any, res: any, next: any) => {
-	res.json(req.file);
-});
+	res.json(req.file)
+})
 
-let port = process.env.PORT || 3000;
+let port = process.env.PORT || 3000
 http.listen(port, () => {
-	console.log(`Listening on port ${port}`);
-});
+	console.log(`Listening on port ${port}`)
+})
